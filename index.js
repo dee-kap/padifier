62d1739f19937bcea283c96861d59e9dbcfe4cff
Deepak Kapoor <kapoordeepak@gmail.com>
2016-03-24 16:49:12 +1100

    pads left, right and both

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-'use strict';
+(function() {
+  'use strict';
 
-module.exports = {
-  padLeft: function(str, length, padCharacter) {
-    str = String(str);
+  function getPadding(length, padCharacter) {
     length = length ? length : 0;
     padCharacter = padCharacter ? padCharacter : ' ';
 
@@ -10,13 +9,29 @@ module.exports = {
     for (var i = 0; i < length; i++) {
       temp += padCharacter;
     }
-    str = temp.concat(str);
-
-    return str;
-  },
-
-  padRight: function() {
+    return temp;
   }
-};
 
+  var _padLeft = function(str, length, padCharacter) {
+    str = String(str);
+    str = getPadding(length, padCharacter).concat(str);
+    return str;
+  };
 
+  var _padRight = function(str, length, padCharacter) {
+    str = String(str);
+    str = str.concat(getPadding(length, padCharacter));
+    return str;
+  };
+
+  var _padBoth = function(str, length, padCharacter) {
+    var padding = getPadding(length, padCharacter);
+    return padding.concat(str).concat(padding);
+  };
+
+  module.exports = {
+    padLeft: _padLeft,
+    padRight: _padRight,
+    padBoth: _padBoth
+  };
+})();
--- test.js
+++ test.js
@@ -22,6 +22,14 @@ describe('padifier tests', function() {
     assert.isTrue(_.isFunction(padifier.padRight));
   });
 
+  it('should define padBoth', function() {
+    assert.isFalse(_.isUndefined(padifier.padBoth));
+  });
+
+  it('should define padBoth as a function', function() {
+    assert.isTrue(_.isFunction(padifier.padBoth));
+  });
+
   describe('.padLeft', function() {
 
     it('should return the string as is when length and pad-character are not passed in', function() {
@@ -102,4 +110,43 @@ describe('padifier tests', function() {
 
   });
 
+  describe('.padBoth', function() {
+
+    it('should return the string as is when length and pad-character are not passed in', function() {
+      var expected = "hello";
+      var actual = padifier.padBoth('hello');
+      assert.equal(expected, actual);
+    });
+
+    it('should return the string as is when length is zero', function() {
+      var expected = "hello";
+      var actual = padifier.padBoth('hello', 0);
+      assert.equal(expected, actual);
+    });
+
+    it('should return the string as is when undefined length is passed in', function() {
+      var expected = "hello";
+      var actual = padifier.padBoth('hello', undefined, '*');
+      assert.strictEqual(expected, actual);
+    });
+
+    it('should pad empty spaces when padCharacter is empty', function() {
+      var expected = "     hello     ";
+      var actual = padifier.padBoth('hello', 5, '');
+      assert.strictEqual(expected, actual);
+    });
+
+    it('should pad empty spaces when padCharacter is undefined', function() {
+      var expected = "         hello         ";
+      var actual = padifier.padBoth('hello', 9 );
+      assert.strictEqual(expected, actual);
+    });
+
+    it('should pad correctly', function() {
+      var expected = "$$$$$$hello$$$$$$";
+      var actual = padifier.padBoth('hello', 6, '$' );
+      assert.strictEqual(expected, actual);
+    });
+
+  });
 });
